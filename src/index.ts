f653cf13f8d5663fea42061aeb7222664ac7f7e9
philipjonlewis <philipjonlewis@gmail.com>
2022-01-10 17:26:16 +0800

    imported another ts file in the main file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import "./sass/style.scss";
 
+import trialFunction from "./ts/about";
+
 const btn = document.querySelector(".sample")!;
 
 btn.addEventListener("click", () => {
+  trialFunction();
   console.log("sample");
 });
---
+++ src/ts/about.ts
@@ -0,0 +1,7 @@
+console.log(
+    'About page ts file'
+  );
+  
+  export default () => {
+    console.log('This is the about page ts file');
+  };
