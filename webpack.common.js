eea847c735df8144a8b020167c19eb12e3e77b37
philipjonlewis <philipjonlewis@gmail.com>
2022-01-10 17:55:40 +0800

    cleaned up unused loaders

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: {
     main: "./src/index.ts",
-    vendor : "./src/vendors/vendor.ts"
+    vendor: "./src/vendors/vendor.ts",
   },
   module: {
     rules: [
@@ -12,10 +12,10 @@ module.exports = {
         use: "ts-loader",
         exclude: /node_modules/,
       },
-      {
-        test: /\.html$/,
-        use: ["html-loader"],
-      },
+      // {
+      //   test: /\.html$/,
+      //   use: ["html-loader"],
+      // },
       {
         test: /\.(svg|png|jpg|gif)$/,
         type: "asset/resource",
