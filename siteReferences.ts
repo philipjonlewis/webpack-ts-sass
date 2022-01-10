d89ea8f8c264da2ece42ff77ec8327c64b2ae2db
philipjonlewis <philipjonlewis@gmail.com>
2022-01-10 18:36:23 +0800

    added a separate file for site pagge references

---
+++ siteReferences.ts
@@ -0,0 +1,22 @@
+const indexPage = {
+  filename: "index.html",
+  template: "./src/views/index.ejs",
+  name: "this is the home",
+  siteName: "Philip's Website",
+};
+
+const aboutPage = {
+  filename: "about.html",
+  template: "./src/views/pages/about.ejs",
+  name: "this is the about",
+  siteName: "Philip's Website | About",
+};
+
+const contactPage = {
+  filename: "contact.html",
+  template: "./src/views/pages/contact.ejs",
+  name: "this is the contact",
+  siteName: "Philip's Website | Contact",
+};
+
+export { indexPage, aboutPage, contactPage };
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,7 @@ const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlWebpackPartialsPlugin = require("html-webpack-partials-plugin");
-
+const { indexPage, aboutPage, contactPage } = require("./siteReferences");
 module.exports = merge(common, {
   mode: "development",
   output: {
@@ -11,24 +11,9 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./src/views/index.ejs",
-      name: "this is the home",
-      siteName: "Philip's Website",
-    }),
-    new HtmlWebpackPlugin({
-      filename: "about.html",
-      template: "./src/views/pages/about.ejs",
-      name: "this is the about",
-      siteName: "Philip's Website | About",
-    }),
-    new HtmlWebpackPlugin({
-      filename: "contact.html",
-      template: "./src/views/pages/contact.ejs",
-      name: "this is the contact",
-      siteName: "Philip's Website | Contact",
-    }),
+    new HtmlWebpackPlugin(indexPage),
+    new HtmlWebpackPlugin(aboutPage),
+    new HtmlWebpackPlugin(contactPage),
     new HtmlWebpackPartialsPlugin({
       path: path.join(__dirname, "./src/views/partials/header.ejs"),
       location: "header",
